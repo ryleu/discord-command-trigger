c27dc173b0f42f352975bb35797424efeedd0514
ryleu <69326171+ryleu@users.noreply.github.com>
2023-07-18 03:05:13 -0400

    completed all of the base functionality

---
+++ main.py
@@ -0,0 +1,61 @@
+import discord
+import json
+import os
+import io
+
+working_directory = os.getcwd()
+
+config = {}
+with open("config.json") as file:
+    config = json.loads(file.read())
+
+# config.json example
+"""
+{
+    "token": "token here",
+    "owner_ids": [
+        587967909641584661,
+        600130839870963725
+    ],
+    "channel_ids": [
+        795651596709003295
+    ]
+}
+"""
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if (
+        message.author == client.user
+        or message.channel.id not in config["channel_ids"]
+        or message.author.id not in config["owner_ids"]
+    ):
+        return
+
+    flows = {}
+    with open("flows.json") as file:
+        flows = json.loads(file.read())
+
+    for name in flows:
+        if message.content.startswith(name):
+            os.system(f"cd {working_directory}")
+            msg = os.popen(" ; ".join(flows[name])).read()
+
+            await message.channel.send(
+                "command output:",
+                file=discord.File(filename="output.txt", fp=io.BytesIO(msg.encode())),
+            )
+
+
+client.run(config["token"])
