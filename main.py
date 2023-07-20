59ff7203420c9a716cd00d478d4e24865daf2cbf
ryleu <69326171+ryleu@users.noreply.github.com>
2023-07-20 02:05:09 -0400

    comment my code

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import json
 import os
 import io
 
-working_directory = os.getcwd()
-
+# pull the config data. this does not need to be updated live
 config = {}
 with open("config.json") as file:
     config = json.loads(file.read())
@@ -23,6 +22,7 @@ with open("config.json") as file:
 }
 """
 
+# this bot requires the message content intent
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -31,26 +31,26 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"We have logged in as {client.user}")
+    print(f"logged in as {client.user}")
 
 
 @client.event
 async def on_message(message):
+    # make sure the message is from an valid user in a valid channel
     if (
-        message.author == client.user
-        or message.channel.id not in config["channel_ids"]
+        message.channel.id not in config["channel_ids"]
         or message.author.id not in config["owner_ids"]
     ):
         return
 
+    # we don't store the flows because we want to be able to live update them
     flows = {}
     with open("flows.json") as file:
         flows = json.loads(file.read())
 
     for name in flows:
         if message.content == name:
-            os.system(f"cd {working_directory}")
-
+            # split out all of the data from the dict to variables for easy access
             flow = flows[name]
             steps = flow.get(
                 "steps", [f"echo 'The flow `{name}` has no associated steps.'"]
@@ -59,14 +59,20 @@ async def on_message(message):
             description = flow.get("description", "*No description.*")
             to_run = "\n".join(steps)
 
+            # initial message informing the user what they just ran
             await message.channel.send(
                 f"## description:\n> {description}\n"
                 + f"## running commands:\n```sh\n{to_run}\n```"
             )
 
+            # combine all steps into a single command
             msg = os.popen(" ; ".join(steps)).read()
 
+            # we send it as a file if the response is over 1000 characters
+            # so that it doesn't take up too much space
             if len(msg) < 1000:
+                # special exception for the `discord` format to allow for
+                # pings and channel mentions
                 if file_format == "discord":
                     await message.channel.send(
                         f"## command output:\n{msg}",
